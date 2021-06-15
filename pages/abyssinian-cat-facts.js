0f6d0866953ccbb8b336796e6661207e2eef80d7
Viktorya Hakobyan <victoria@webmaker.am>
2021-06-15 21:37:18 +0400

    Animals

--- pages/abyssinian-cat-facts.js
+++ pages/abyssinian-cat-facts.js
@@ -22,12 +22,87 @@ const AbyssinianCat = ({animal}) => {
             <div className="hero-wrapper">
                 <img src={animal.hero_image} alt={animal.hero_alt_text} className="article-hero-image"/>
             </div>
-            <div className="container-article-text">{animal.introduction}</div>
+            <div className="container-article-text">
+                <div dangerouslySetInnerHTML={{__html:animal.introduction}}/>
+            </div>
             <div className="fact-file-container">
-                <h2 className="fact-file-top-heading">Abyssinian cat</h2>
+                <h2 className="fact-file-top-heading">{animal.animal_name} Fact File</h2>
                 <div className="fact-file-row">
-                    <div className="fact-file-column-1"> </div>
+                    <div className="fact-file-column-1">
+                        <div className="fact-container">
+                            <h4 className="fact-question">What do they prey on?</h4>
+                            <p className="fact-answer">{animal.prey_on}</p>
+                        </div>
+                        <div className="fact-container">
+                            <h4 className="fact-question">What do they eat?</h4>
+                            <p>{animal.eat}</p>
+                        </div>
+                        <div className="fact-container">
+                            <h4 className="fact-question">Average litter size?</h4>
+                            <p>{animal.litter_size}</p>
+                        </div>
+                        <div className="fact-container">
+                            <h4 className="fact-question">How much do they weigh?</h4>
+                            <p>{animal.weigh}</p>
+                        </div>
+                        <div className="fact-container">
+                            <h4 className="fact-question">How long are they?</h4>
+                            <p>{animal.long}</p>
+                        </div>
+                        <div className="fact-container">
+                            <h4 className="fact-question">How tall are they?</h4>
+                            <p>{animal.tall}</p>
+                        </div>
+                        <div className="fact-container">
+                            <h4 className="fact-question">What do they look like?</h4>
+                            <p>{animal.look_like}</p>
+                        </div>
+                        <div className="fact-container">
+                            <h4 className="fact-question">Skin Type</h4>
+                            <p>{animal.skin_type}</p>
+                        </div>
+                        <div className="fact-container">
+                            <h4 className="fact-question">What are their main threats?</h4>
+                            <p>{animal.main_threats}</p>
+                        </div>
+                        <div className="fact-container">
+                            <h4 className="fact-question">What is their conservation status?</h4>
+                            <p>{animal.conservation_status}</p>
+                        </div>
+                        <div className="fact-container">
+                            <h4 className="fact-question">Where you'll find them</h4>
+                            <p>{animal.where_find}</p>
+                        </div>
+                        <div className="fact-container">
+                            <h4 className="fact-question">Locations</h4>
+                            <p>{animal.locations}</p>
+                        </div>
+                        <div className="fact-container">
+                            <h4 className="fact-question">Kingdom</h4>
+                            <p>{animal.kingdom}</p>
+                        </div>
+                        <div className="fact-container">
+                            <h4 className="fact-question">Class</h4>
+                            <p>{animal.class}</p>
+                        </div>
+                        <div className="fact-container">
+                            <h4 className="fact-question">Scientific Name</h4>
+                            <p>{animal.scientific_name}</p>
+                        </div>
+                        <div className="fact-container">
+                            <h4 className="fact-question">Family</h4>
+                            <p>{animal.family}</p>
+                        </div>
+                        <div className="fact-container">
+                            <h4 className="fact-question">Genus</h4>
+                            <p>{animal.genus}</p>
+                        </div>
+
+                    </div>
                 </div>
+
+                <div dangerouslySetInnerHTML={{__html:animal.body_copy}}/>
+
             </div>
         </div>
     )
